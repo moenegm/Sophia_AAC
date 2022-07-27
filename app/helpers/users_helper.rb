ba94cd9c9ec64b3bdda683f91d112bd0d4cc4160
Mohamed A. Negm <mr.negm90@gmail.com>
2022-07-27 02:27:40 -0700

    avatar in user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
         if user.avatar.present?
             link_to image_tag(current_user.avatar, class:"avatar mb-3", radius: 0)
         else
-            link_to image_tag("default_avatar.jpg", class:"mb-3 avatar justify-items-center", radius: 0)
+            link_to image_tag("default-avatar.jpg", class:"mb-3 avatar justify-items-center", radius: 0)
         end
     end
 
